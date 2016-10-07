880245ef5e580efb9a91701114e3b423b42d3c5d
UselessMnemonic <chrisjmadrigal@gmail.com>
2016-10-06 22:40:32 -0700

    Added Paste, Replace, and Locate String; new Globals; changed harambe to vector. PLEASE adapt any code to new master project

--- TextEditorGroup3/TextEditor/TextEditor.cpp
+++ TextEditorGroup3/TextEditor/TextEditor.cpp
@@ -4,6 +4,7 @@
 */
 #include "stdafx.h"
 #include <iostream>
+#include <vector>
 #include <string>
 using namespace std;
 
@@ -14,7 +15,7 @@ void type(int linesToPrint);//reyes
 
 void deleteLines(int numberOfLines);//kevin
 void moveToLine(int lineToMoveTo);//kevin
-void instertLines(int linesToInsert);//kevin
+void insertLines(int linesToInsert);//kevin
 
 void replace(int linesToReplace);//chris
 void locateString(string stringToFind);//chris
@@ -29,16 +30,87 @@ void moveUp(); //etc.
 void moveDown(); //etc.
 
 //team globals
-string harambe[]; //text body
+vector<string> harambe; //text body
+vector<string> storedCopy;
 bool fileEdited; //true if file has been edited since load or last save; false otherwise
+int currentLine;
 
 //getters/setters
 bool getFileEdited(); //get value of fileEdited
 void setFileEdited(bool value); //set value of fileEdited
-
+void init(); //initialize globals
+int getCurrentLine(); //get current line
+void setCurrentLine(int value); //set the value of currentLine
 
 int main() //use this to test your functions
 {
-	return 0;
 }
 
+void replace(int linesToReplace)
+{
+	if (linesToReplace < 0)
+		return;
+	int currLine = getCurrentLine();
+	for (int x = currLine; x < currLine + linesToReplace; x++)
+	{
+		try
+		{
+			harambe.assign(x, "");
+		}
+		catch (out_of_range ofr)
+		{
+			break; //break out of for loop--elements have been replaced.
+		}
+	}
+
+}
+
+void locateString(string stringToFind)
+{
+	if (stringToFind.compare(""))
+		return;
+
+	string testString;
+	for (unsigned int x = getCurrentLine(); x < harambe.size(); x++)
+	{
+		testString = harambe.at(x);
+		if (testString.find(stringToFind) != string::npos)
+		{
+			break;
+		}
+	}
+}
+
+void paste()
+{
+	vector<string>::iterator it = harambe.begin();
+	for(unsigned int x = 0; x < storedCopy.size(); x++)
+		harambe.insert(it+getCurrentLine()+x, storedCopy.at(x));
+}
+
+void init()
+{
+	harambe = vector<string>();
+	storedCopy = vector<string>();
+	setFileEdited(false);
+}
+
+bool getFileEdited()
+{
+	return fileEdited;
+}
+
+void setFileEdited(bool value)
+{
+	fileEdited = value;
+}
+
+int getCurrentLine()
+{
+	return currentLine;
+}
+
+void setCurrentLine(int value)
+{
+	currentLine = value;
+}
