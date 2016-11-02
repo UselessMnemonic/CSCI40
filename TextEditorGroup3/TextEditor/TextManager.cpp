509544a9a13095d13b955948ae46adabbdde5c98
UselessMnemonic <chrisjmadrigal@gmail.com>
2016-11-01 19:48:23 -0700

    I didn't know adding changes and uploading were different

--- TextEditorGroup3/TextEditor/TextManager.cpp
+++ TextEditorGroup3/TextEditor/TextManager.cpp
@@ -33,21 +33,33 @@ void TextManager::setCurrentLine(int value)
 	currentLine = value;
 }
 
+//REYES
+void TextManager::type(int numLinesToType)
+{
+	int res = (harambe.size() - getCurrentLine());
+	if (numLinesToType > res)
+		numLinesToType = res;
+	for (int x = 0; x < numLinesToType; x++)
+	{
+		cout << harambe.at(getCurrentLine() + x) << endl;
+	}
+}
+
 //CHRIS
 void TextManager::replace(int numLinesToReplace)
 {
 	if (numLinesToReplace < 0) //make sure user input is not negative number
 		return;
 
-	int currLine = getCurrentLine();
-
-	if (unsigned int(numLinesToReplace + currLine) > harambe.size()) //if number of lines to replace exceeds the size of the text body
-		numLinesToReplace = harambe.size() - currLine;//then we set the number to how many are left
-
-	for (int x = currLine; x < currLine + numLinesToReplace; x++)//for the lines specified
+	if (unsigned int(numLinesToReplace + getCurrentLine()) > harambe.size()) //if number of lines to replace exceeds the size of the text body
+		numLinesToReplace = harambe.size() - getCurrentLine();//then we set the number to how many are left
+	string input;
+	for (int x = 0; x < numLinesToReplace; x++)//for the lines specified
 	{
-		harambe.assign(x, ""); //set the line to blank
+		getline(cin, input);
+		harambe.assign(getCurrentLine()+x, input); //set the line to blank
 	}
+	setCurrentLine(numLinesToReplace + getCurrentLine() - 1);
 }
 
 void TextManager::locateString(string stringToFind)
@@ -82,23 +94,36 @@ void TextManager::insertLines(int linesToinsert) //should be OK
 	if(linesToinsert <= 0)
 		cout << "You must input a number above 0" << endl;
 		return;
-	
-		if(getCurrentLine() == harambe.size)
-		{
-			harambe.push_back("");
-			linesToinsert--;
-		}
 
-	for(int i = 1; i <= linesToinsert; i++)
+		string input;
+	if(getCurrentLine() == harambe.size()-1)
 	{
-		harambe.insert(harambe.begin() + getCurrentLine()+1, ""); //writes the lines in the array to the text body 
-		setCurrentLine(getCurrentLine()+1);
+		for (int i = 1; i <= linesToinsert; i++)
+		{
+			getline(cin, input);
+			harambe.push_back(input);
+			setCurrentLine(getCurrentLine() + 1);
+		}
 	}
-
+	else
+	{
+		for (int i = 1; i <= linesToinsert; i++)
+		{
+			getline(cin, input);
+			harambe.insert(harambe.begin() + getCurrentLine() + 1, input); //writes the lines in the array to the text body 
+			setCurrentLine(getCurrentLine() + 1);
+		}
+	}
 
 }
 
 void TextManager::moveToLine(int lineToMove)
+{
+	if(lineToMove > -1 && lineToMove < harambe.size())
+	setCurrentLine(lineToMove);
+}
+
+/*void TextManager::moveToLine(int lineToMove)
 {
 	int lineNum;
 	
@@ -108,14 +133,10 @@ void TextManager::moveToLine(int lineToMove)
 	
 	harambe.push_back(harambe[lineNum + 1]);
 	harambe.erase(harambe.begin() + (lineNum + 1)); 
-}
+}*/
 
 void TextManager::deleteLines(int linesToDelete)
 {
-	int lineNum;
 
-	cout << "Starting from which line do you want to delete from?" << endl;
-	cin >> lineNum;
-
-	harambe.erase(harambe.begin() + lineNum, harambe.begin() + lineNum + linesToDelete);
+	harambe.erase(harambe.begin() + getCurrentLine(), harambe.begin() + getCurrentLine() + linesToDelete-1);
 }
